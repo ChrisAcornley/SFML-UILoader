3b32f0a0869cee2b53cbbc5853dd6f62765cf898
Christopher Acornley <chrisacornley@hotmail.co.uk>
2019-04-04 13:59:01 +0100

    Added main to project

--- SFML-UILoader/SFML-UILoader/main.cpp
+++ SFML-UILoader/SFML-UILoader/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Program running" << std::endl;
-	return 0;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Works!");
+	sf::CircleShape circle(100.f);
+	circle.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(circle);
+		window.display();
+	}
 }
